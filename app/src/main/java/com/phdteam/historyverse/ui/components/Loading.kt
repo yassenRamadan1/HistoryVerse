acdb03976ea52b57104b3a9504fc841449fe3b9c
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2024-04-30 23:18:42 +0300

    fix naming

--- app/src/main/java/com/phdteam/historyverse/ui/components/Loading.kt
+++ app/src/main/java/com/phdteam/historyverse/ui/components/Loading.kt
@@ -1,4 +1,4 @@
-package com.solutionteam.design_system.components
+package com.phdteam.historyverse.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
