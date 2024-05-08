97647e77b2c038c1be94e5877442a10a42ad6568
kareem <kareemesam52@yahoo.com>
2024-05-08 23:15:17 +0300

    modify route

--- app/src/main/java/com/phdteam/historyverse/ui/presentation/main/navigation/Route.kt
+++ app/src/main/java/com/phdteam/historyverse/ui/presentation/main/navigation/Route.kt
@@ -15,19 +15,20 @@ const val navigationRouteMain = "main"
 const val navigationRouteHome = "home"
 const val navigationRouteSearch = "search"
 const val navigationRouteProfile = "profile"
+const val navigationRouteDetails = "details"
 
 
 sealed class Screen(
-    val route: String,
-    var routePath: String? = null,
-    var clearBackStack: Boolean = false,
-    val restoreState: Boolean = true,
-    val title: Int = 0,
-    val icon: ImageVector? = null
+    val route : String ,
+    var routePath : String? = null ,
+    var clearBackStack : Boolean = false ,
+    val restoreState : Boolean = true ,
+    val title : Int = 0 ,
+    val icon : ImageVector? = null
 ) {
     fun withClearBackStack() = apply { clearBackStack = true }
 
-    fun routeWith(path: String) = apply {
+    fun routeWith(path : String) = apply {
         routePath = path
     }
 
@@ -36,18 +37,24 @@ sealed class Screen(
 
     object Main : Screen(navigationRouteMain)
     object Home : Screen(
-        route = navigationRouteHome,
-        title = R.string.home_title,
+        route = navigationRouteHome ,
+        title = R.string.home_title ,
         icon = Icons.Rounded.Home
     )
+
     object Search : Screen(
-        route = navigationRouteSearch,
-        title = R.string.search_title,
+        route = navigationRouteSearch ,
+        title = R.string.search_title ,
         icon = Icons.Rounded.Search
     )
+
     object Profile : Screen(
-        route = navigationRouteProfile,
-        title = R.string.profile_title,
+        route = navigationRouteProfile ,
+        title = R.string.profile_title ,
         icon = Icons.Rounded.Person
+    )
+
+    object Details : Screen(
+        route = navigationRouteDetails ,
     )
 }
