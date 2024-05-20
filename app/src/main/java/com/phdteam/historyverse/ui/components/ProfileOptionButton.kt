47409b4a1b928afaf5b029595e8cd299763a5ff0
Garfend <71102453+Garfend@users.noreply.github.com>
2024-05-20 18:14:51 +0300

    modify button

--- app/src/main/java/com/phdteam/historyverse/ui/components/ProfileOptionButton.kt
+++ app/src/main/java/com/phdteam/historyverse/ui/components/ProfileOptionButton.kt
@@ -3,21 +3,21 @@ package com.phdteam.historyverse.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.phdteam.historyverse.ui.modifier.noRippleEffect
 
 
 @Composable
@@ -27,32 +27,41 @@ fun ProfileOptionButton(
     painter: Int,
     modifier: Modifier = Modifier
 ) {
-    Button(
-        onClick = onClickOption,
+
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .height(64.dp),
-        colors = ButtonDefaults.buttonColors(Color.White),
-
-        ) {
-        Row(
+            .height(64.dp)
+            .noRippleEffect { onClickOption() }
+            .padding(
+                horizontal = 16.dp,
+                vertical = 16.dp
+            )
+            ,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = text,
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Normal
+        )
+        Image(
+            painter = painterResource(id = painter),
+            contentDescription = "",
             modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = text,
-                color = Color.Black,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Normal
-            )
-            Image(
-                painter = painterResource(id = painter),
-                contentDescription = "",
-                modifier = Modifier
-                    .graphicsLayer(rotationZ = 180.0F)
-            )
-        }
+                .graphicsLayer(rotationZ = 180.0F)
+        )
     }
+}
+
+@Preview
+@Composable
+fun ProfileOptionButtonPreview() {
+    ProfileOptionButton(
+        text = "Option",
+        onClickOption = {},
+        painter = android.R.drawable.arrow_down_float
+    )
 }
