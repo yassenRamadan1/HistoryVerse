2b256e77876e01274d126f2790345deb539916a2
Garfend <71102453+Garfend@users.noreply.github.com>
2024-06-27 23:15:45 +0300

    payment screen

--- app/src/main/java/com/phdteam/historyverse/ui/presentation/payment/PaymentUIEffect.kt
+++ app/src/main/java/com/phdteam/historyverse/ui/presentation/payment/PaymentUIEffect.kt
@@ -1,7 +1,5 @@
 package com.phdteam.historyverse.ui.presentation.payment
 
-import com.phdteam.historyverse.ui.presentation.profile.ProfileUIEffect
-
 sealed interface PaymentUIEffect {
     object PaymentError : PaymentUIEffect
 }
