cfcb5b6be45866e0bb4b12eee356f845f2ea3343
kareem <kareemesam52@yahoo.com>
2024-06-28 23:31:28 +0300

    update market item details viewModel

--- app/src/main/java/com/phdteam/historyverse/ui/presentation/market/marketDetails/MarketItemDetailsViewModel.kt
+++ app/src/main/java/com/phdteam/historyverse/ui/presentation/market/marketDetails/MarketItemDetailsViewModel.kt
@@ -1,15 +1,36 @@
 package com.phdteam.historyverse.ui.presentation.market.marketDetails
 
+import com.phdteam.historyverse.data.network.model.Artifact
+import com.phdteam.historyverse.data.network.repositories.HistoryVerseRepository
 import com.phdteam.historyverse.ui.presentation.base.BaseViewModel
-import com.phdteam.historyverse.ui.presentation.market.MarketUiEffect
 
-class MarketItemDetailsViewModel(private val itemId: Int) :
+class MarketItemDetailsViewModel(
+    private val itemId: Int?,
+    private val repository: HistoryVerseRepository
+) :
     BaseViewModel<MarketItemDetailsUiState, MarketDetailsUiEffect>(MarketItemDetailsUiState()) {
+
     init {
         getItemDetails()
     }
 
-    fun getItemDetails() {}
+    fun getItemDetails() {
+        tryToExecute(
+            {
+                repository.getArtifactById(itemId ?: 2352)
+            }, ::onSuccess,
+            ::onError
+        )
+    }
+
+    private fun onError() {
+        updateState { it.copy(isError = true, isLoading = false) }
+    }
+
+    fun onSuccess(artifact: Artifact) {
+//        updateState { it.copy(artifact = artifact.toMarketItemDetailsUiState()) }
+    }
+
     fun addToCart(itemId: Int) {
         //
     }
